856c52e073d2370ddfa5d46f0b2b1af044a8485e
Sergey Zaycev <s.zaycev.dev@gmail.com>
2023-06-17 13:28:51 +0300

    rename

--- Package.swift
+++ Package.swift
@@ -4,12 +4,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "Cabbage",
+    name: "SZCabbage",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "Cabbage",
-            targets: ["Cabbage"]),
+            name: "SZCabbage",
+            targets: ["SZCabbage"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,18 +19,8 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "Cabbage",
-            dependencies: [],
-            linkerSettings: [
-                .linkedFramework(
-                    "AVFoundation",
-                    .when(platforms: [.iOS, .macOS])
-                ),
-                .linkedFramework(
-                    "Foundation",
-                    .when(platforms: [.iOS, .macOS])
-                )
-            ]
+            name: "SZCabbage",
+            dependencies: []
         )
     ]
 )
